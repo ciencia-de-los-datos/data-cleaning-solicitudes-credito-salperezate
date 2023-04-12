f57539c0bdad5103eb7d87937270ad09ab81543b
salperezate <salperezate@unal.edu.co>
2023-04-11 21:23:45 -0500

    Quiz 5

--- pregunta.py
+++ pregunta.py
@@ -6,15 +6,33 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+from dateutil.parser import parse
 import pandas as pd
 
 
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
+    df.reset_index(inplace=True,drop=True)
+    
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True, format='mixed')
+    
+    df.dropna(axis='index',inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    #
-    # Inserte su código aquí
-    #
+    df['sexo'] = df['sexo'].str.lower().astype(str).str.strip()
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower().astype(str)
+    df['idea_negocio'] = df['idea_negocio'].str.lower().astype(str)
+    df['barrio'] = df['barrio'].str.lower().astype(str)
+    df['línea_credito'] = df['línea_credito'].str.lower().astype(str)
+     
+    df['idea_negocio'] = df['idea_negocio'].str.replace('_',' ').str.replace('-',' ').str.strip()
+    df['barrio'] = df['barrio'].str.replace('_','-').str.replace('-',' ')
+    df['línea_credito'] = df['línea_credito'].str.replace('_',' ').str.replace('-',' ').str.strip()
+
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(',','').str.replace('$','',regex=False).str.replace(' ','').str.strip().astype(float)   
+    
+    df.drop_duplicates(inplace=True)
+    df.dropna(axis='index',inplace=True)
 
     return df
